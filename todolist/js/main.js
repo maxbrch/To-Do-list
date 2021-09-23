4d66ee8e346ec0e9dc957b49dd2f590f0076a4e3
Getvion <gog0125@mail.com>
2021-09-23 14:35:17 +0300

    добавил появление модального она при клике на кнопку show

--- todolist/js/main.js
+++ todolist/js/main.js
@@ -1,4 +1,4 @@
-let
+const
   $btn = document.querySelector('.add-controls_btn'),
   $inp = document.querySelector('.add-controls_inp'),
   $out = document.querySelector('.out'),
@@ -8,21 +8,22 @@ let
   sidebar = document.getElementById("mySidenav");
 // зірка
 function star() {
-   var starx = document.bodycreateElement('star');
-   starx.classList.toggle(".m-on");
+  const starx = document.bodycreateElement('star');
+  starx.classList.toggle(".m-on");
 }
 document.addEventListener('click', checkPriority);
-function checkPriority(e){
+
+function checkPriority(e) {
   let
     $target = e.target,
     targetClasses = $target.className.split(' ');
 
-  console.log('--| target');
-  console.log(targetClasses);
+  // console.log('--| target');
+  // console.log(targetClasses);
 
   let isRight = targetClasses.indexOf('out_task_star');
 
-  if(!(isRight<0)){
+  if (!(isRight < 0)) {
     console.log('--| change star!');
 
     $target.classList.add("m-on");
@@ -30,67 +31,71 @@ function checkPriority(e){
 }
 // добавлення задачі
 $btn.addEventListener('click', addTask);
-function addTask(e){
+function addTask(e) {
   let taskName = $inp.value;
-  let dates    =  date.value;
-  console.log('--| task name: ' + taskName);
-// перевірка на попрожню строку
-  if( taskName!='' && taskName!=' ' && taskName!='  '){
+  let dates = date.value;
+  console.log(`--| task name: ${taskName}`);
+  // перевірка на попрожню строку
+  if (taskName != '' && taskName != ' ' && taskName != '  ') {
     console.log(` -| name is right`);
-// добавлення задачі
+    // добавлення задачі
 
-$btn.addEventListener('click', addTask);
-let task   = document.createElement('div');
-task.className     = 'task';
-task.id            = taskName;
-$out.append(task); 
+    $btn.addEventListener('cliсck', addTask);
+    const task = document.createElement('div');
+    task.className = 'task';
+    task.id = taskName;
+    $out.append(task);
 
+    const taskN = document.createElement('p');
+    const check = document.createElement('input');
+    const button = document.createElement('button');
+    const btnshow = document.createElement('button');
+    check.type = "checkbox";
+    button.textContent = 'Remove';
+    btnshow.textContent = 'Show';
+    btnshow.classList.add('btn__show');
 
-let taskN  = document.createElement('p');
-let check  = document.createElement('input')
-let button = document.createElement('button')
-let btnshow = document.createElement('button');
-check.type = "checkbox";
-button.textContent = 'Remove';
-btnshow.textContent = 'Show';
+    //star
+    let star = new Image();
+    // star.src = 'E:/sendbox/todolist/img/staroff.jpg';
+    star.src = '../img/staroff.jpg';
+    star.addEventListener('click', () => {
+      // star.src = 'E:/sendbox/todolist/img/staron.jpg';
+      star.src = '../img/staron.jpg';
+    });
 
-//star
-let star   = new Image();
-star.src ='E:/sendbox/todolist/img/staroff.jpg';
-star.addEventListener('click', starClass);
-function starClass() {
-  star.src ='E:/sendbox/todolist/img/staron.jpg';
+    function subTask() {
+      console.log("--| subTask");
+    } // sub task
+    // function myFunction() {
+    //   var popup = document.getElementById("myPopup");
+    //   popup.classList.toggle("show");
+    // }
+
+    btnshow.addEventListener('click', subTask);
+
+    button.addEventListener('click', () => {
+      button.remove(),
+        task.remove(),
+        check.remove(),
+        btnshow.remove()
+    });
+    task.append(star, taskN, check, button, btnshow);
+    taskN.textContent = taskName;
+    btnshow.addEventListener('click', () => {
+      // по нажатию на кнопку вызывается поп ап и раскрывается карточка 
+      const modalWindow = document.querySelector('.modal');
+      modalWindow.style.display = 'block';
+      const modalClose = document.querySelector('.modal__close');
+      modalClose.addEventListener('click', () => {
+        modalWindow.style.display = 'none';
+      });
+    });
+  }
+
 }
 
-function subTask() {
-  console.log("--| subTask")
-} // sub task
- function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-}
 
-btnshow.addEventListener('click', subTask);
-
-button.addEventListener('click', ()=>{  button.remove(),task.remove(),check.remove(),btnshow.remove() });
-task.append(star,taskN,check,button,btnshow); 
-taskN.textContent   = taskName;
-    btnshow.addEventListener('click', ()=>{myFunction()});
- /*  var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");*/
-
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-}
-}
-
-}
-
-/*btnshow.addEventListener('click', ()=>{
-  console.log("sub")
-});
-*/
 /*
 function subTask() {
   console.log("--| subTask")
@@ -106,6 +111,6 @@ function closeNavt() {
 }
 // dark theme
 function theme() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
-} 
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
